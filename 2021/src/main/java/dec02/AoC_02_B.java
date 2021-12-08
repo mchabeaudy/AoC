1b4a238781f145c44cfe275bf566c245b39103fa
Matthieu <matthieu.chabeaudy@gmail.com>
2021-12-08 23:46:49 +0100

    initial

---
+++ 2021/src/main/java/dec02/AoC_02_B.java
@@ -0,0 +1,33 @@
+package dec02;
+
+import static java.lang.ClassLoader.getSystemResource;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class AoC_02_B {
+
+  private static final String FILE = "dec02.txt";
+
+  public static void main(String[] args) throws Exception {
+    var lines = Files.lines(Paths.get(getSystemResource(FILE).getPath().substring(1)));
+    var h = new AtomicLong(0);
+    var v = new AtomicLong(0);
+    var aim = new AtomicLong(0);
+    lines.forEach(l -> {
+      var in = l.split(" ");
+      long d = Long.parseLong(in[1]);
+      switch (in[0]) {
+        case "forward" -> {
+          h.addAndGet(d);
+          v.addAndGet(aim.get() * d);
+        }
+        case "down" -> aim.addAndGet(d);
+        case "up" -> aim.addAndGet(-d);
+      }
+    });
+    System.out.println(v.get() * h.get());
+  }
+
+}

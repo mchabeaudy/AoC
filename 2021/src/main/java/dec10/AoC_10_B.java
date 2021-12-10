6721539c6e57b81e8244c2cc4aebd262565d5af6
Matthieu Chabeaudy <matthieu.chabeaudy@sap.com>
2021-12-10 11:51:18 +0100

    clear code

--- 2021/src/main/java/dec10/AoC_10_B.java
+++ 2021/src/main/java/dec10/AoC_10_B.java
@@ -1,81 +1,71 @@
 package dec10;
 
 import static java.lang.ClassLoader.getSystemResource;
-import static java.util.function.Predicate.not;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.ArrayDeque;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Stack;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.Map;
 
 public class AoC_10_B {
 
-  private static final String FILE = "dec10.txt";
+    private static final String FILE = "dec10.txt";
 
-  private static final List<String> OPENING = List.of("<", "[", "(", "{");
+    private static final List<String> OPENING = List.of("<", "[", "(", "{");
+    private static final Map<String, Integer> POINTS = Map.of("(", 1, "[", 2, "{", 3, "<", 4);
 
-  public static void main(String[] args) throws Exception {
-    var lines = Files.lines(Paths.get(getSystemResource(FILE).getPath().substring(1)));
 
-    var out = lines.map(AoC_10_B::buildStack)
-        .filter(not(Collection::isEmpty))
-        .peek(Collections::reverse)
-        .map(AoC_10_B::getStackPoints)
-        .sorted()
-        .toList();
-    var index = (out.size()-1)/2;
-    System.out.println(out.get(index));
-  }
-
-  private static Stack<String> buildStack(String line) {
-    var stack = new Stack<String>();
-      for (String s : line.split("")) {
-       if (isOpeningChar(s)) {
-          stack.push(s);
-       } else {
-         var o = stack.pop();
-          if (!isMatchingChar(o, s)) {
-            stack.clear();
-            break;
-          }
+    public static void main(String[] args) {
+        try (var lines = Files.lines(Paths.get(getSystemResource(FILE).getPath().substring(1)))) {
+            var out = lines.map(AoC_10_B::getIncompleteLine)
+                    .filter(Iterator::hasNext)
+                    .map(AoC_10_B::getPoints)
+                    .sorted()
+                    .toList();
+            System.out.println(out.get((out.size() - 1) / 2));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-      }
-      return stack;
     }
 
-  static boolean isOpeningChar(String s) {
-    return OPENING.contains(s);
-  }
-
-  static boolean isMatchingChar(String o, String c) {
-    boolean out = false;
-    switch (o) {
-      case "<" -> out = ">".equals(c);
-      case "{" -> out = "}".equals(c);
-      case "(" -> out = ")".equals(c);
-      case "[" -> out = "]".equals(c);
+    private static Iterator<String> getIncompleteLine(String line) {
+        var deque = new ArrayDeque<String>();
+        for (String s : line.split("")) {
+            if (isOpeningChar(s)) {
+                deque.push(s);
+            } else {
+                var o = deque.pop();
+                if (!isMatchingChar(o, s)) {
+                    deque.clear();
+                    break;
+                }
+            }
+        }
+        return deque.iterator();
     }
-    return out;
-  }
 
-
-  static int getStringPoints(String s) {
-    int out = 0;
-    switch (s) {
-      case "(" -> out = 1;
-      case "[" -> out = 2;
-      case "{" -> out = 3;
-      case "<" -> out = 4;
+    static boolean isOpeningChar(String s) {
+        return OPENING.contains(s);
     }
-    return out;
-  }
 
-  static long getStackPoints(Stack<String> s) {
-    var out = new AtomicLong(0);
-    s.forEach(c -> out.set(out.get() * 5 + getStringPoints(c)));
-    return out.get();
-  }
+    static boolean isMatchingChar(String o, String c) {
+        return switch (o) {
+            case "<" -> ">".equals(c);
+            case "{" -> "}".equals(c);
+            case "(" -> ")".equals(c);
+            case "[" -> "]".equals(c);
+            default -> false;
+        };
+    }
+
+
+    static long getPoints(Iterator<String> s) {
+        long out = 0;
+        while (s.hasNext()) {
+            out = out * 5 + POINTS.get(s.next());
+        }
+        return out;
+    }
 }
